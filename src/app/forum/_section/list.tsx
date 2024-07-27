1fa80a65df11c9ae9adc372310c9c41846b032c5
Nashir Jamali <v-nashir.jamali@btpn.com>
2024-07-27 20:11:46 +0700

    forum list

--- src/app/forum/_section/list.tsx
+++ src/app/forum/_section/list.tsx
@@ -2,22 +2,40 @@
 
 import React, { useEffect, useState } from 'react';
 
-import { Button, Container, IconButton, Stack, TextField } from '@mui/material';
+import {
+  Button,
+  Container,
+  IconButton,
+  Pagination,
+  Stack,
+  TextField
+} from '@mui/material';
 import { Search as SearchIcon } from '@mui/icons-material';
 import ForumListItem from '@/components/ForumListItem';
 import { getForums } from '../handler';
+import { PaginationType } from '@/types'
 
 export default function List() {
   const [forums, setForums] = useState<any[]>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [pagination, setPagination] = useState<PaginationType>({
+    page: 1,
+    pageCount: 1,
+    pageSize: 1,
+    total: 0
+  });
 
   useEffect(() => {
     async function fetchData() {
-      const data = await getForums();
-      setForums(data);
+      const data = await getForums(currentPage);
+      setForums(data.data);
+      setPagination(data.meta.pagination);
     }
 
     fetchData();
-  }, []);
+  }, [currentPage]);
+
+
 
   return (
     <Container
@@ -41,6 +59,13 @@ export default function List() {
           <ForumListItem forum={forum.attributes} key={index} />
         ))}
       </Stack>
+      <Pagination
+        count={pagination.pageCount}
+        variant="outlined"
+        page={pagination.page}
+        onChange={(_e, page) => setCurrentPage(page)}
+        color='primary'
+      />
     </Container>
   );
 }
--- src/app/forum/handler.ts
+++ src/app/forum/handler.ts
@@ -1,8 +1,9 @@
 import axios from '@/configs/axios';
 import { FORUM } from '@/constants/api';
+import { PaginationType } from '@/types'
 
-export const getForums = async () => {
-  const res = await axios.get(FORUM.find);
+export const getForums = async (page: number) => {
+  const res = await axios.get(`${FORUM.find}?pagination[page]=${page}`);
 
-  return res.data.data;
+  return res.data;
 };

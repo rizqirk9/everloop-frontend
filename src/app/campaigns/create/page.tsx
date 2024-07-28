3e442aec907c3d668bcaf75d4b8bb49624a7fcd2
Nashir Jamali <v-nashir.jamali@btpn.com>
2024-07-28 10:12:02 +0700

    fix error navigator

--- src/app/campaigns/create/page.tsx
+++ src/app/campaigns/create/page.tsx
@@ -4,14 +4,18 @@ import Header from './_section/header';
 import Form from './_section/form';
 
 export default function Create() {
-  return (
-    <Container
-      sx={{
-        my: 10
-      }}
-    >
-      <Header />
-      <Form />
-    </Container>
-  );
+  if (typeof navigator !== 'undefined') {
+    return (
+      <Container
+        sx={{
+          my: 10
+        }}
+      >
+        <Header />
+        <Form />
+      </Container>
+    );
+  } else {
+    return <></>;
+  }
 }

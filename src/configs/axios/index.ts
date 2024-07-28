7b0bd1fa81d6c1dfa500695a891c5e70ec674162
nurjayasasongko <afnurjayas@gmail.com>
2024-07-28 10:07:52 +0700

    update axios

--- src/configs/axios/index.ts
+++ src/configs/axios/index.ts
@@ -4,15 +4,20 @@ import errorHandler from './errorHandler';
 import { getFromStorage } from '@/utils/localStorage';
 
 const token = getFromStorage('token');
+const isBrowser = typeof document !== 'undefined';
 
 const instance = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
-  headers: {
-    'X-CSRF-TOKEN': document.head
-      .querySelector('meta[name="csrf-token"]')
-      ?.getAttribute('content'),
-    'Access-Control-Allow-Origin': '*',
-  }
+  headers: isBrowser
+      ? {
+          'X-CSRF-TOKEN': document.head
+            .querySelector('meta[name="csrf-token"]')
+            ?.getAttribute('content'),
+          'Access-Control-Allow-Origin': '*',
+        }
+      : {
+          'Access-Control-Allow-Origin': '*',
+        },
 });
 
 if (token) instance.defaults.headers.common.authorization = `Bearer ${token}`;

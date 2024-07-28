eed5ecdf4a475b2446134f980012163122fb4a15
Nashir Jamali <v-nashir.jamali@btpn.com>
2024-07-28 10:48:56 +0700

    handle use client

--- src/app/forums/create/page.tsx
+++ src/app/forums/create/page.tsx
@@ -1,17 +1,15 @@
+'use client';
+
 import { Container } from '@mui/material';
 import React from 'react';
 import Header from './_section/header';
 import Form from './_section/form';
 
 export default function CreateForum() {
-  if (typeof navigator !== 'undefined') {
-    return (
-      <Container>
-        <Header />
-        <Form />
-      </Container>
-    );
-  } else {
-    return <></>;
-  }
+  return (
+    <Container>
+      <Header />
+      <Form />
+    </Container>
+  );
 }

e56a06ea657137a19872de13f9723a15b4fb80a2
Nashir Jamali <v-nashir.jamali@btpn.com>
2024-07-28 10:52:47 +0700

    fix navigator error

--- src/app/campaigns/create/page.tsx
+++ src/app/campaigns/create/page.tsx
@@ -1,17 +1,29 @@
 'use client'
 
 import { Box, Container } from '@mui/material';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './_section/header';
 import Form from './_section/form';
 
 export default function Create() {
-  <Container
-    sx={{
-      my: 10
-    }}
-  >
-    <Header />
-    <Form />
-  </Container>;
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null;
+  }
+
+  return (
+    <Container
+      sx={{
+        my: 10
+      }}
+    >
+      <Header />
+      <Form />
+    </Container>
+  );
 }
--- src/app/forums/create/page.tsx
+++ src/app/forums/create/page.tsx
@@ -1,11 +1,21 @@
 'use client';
 
 import { Container } from '@mui/material';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './_section/header';
 import Form from './_section/form';
 
 export default function CreateForum() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null;
+  }
+
   return (
     <Container>
       <Header />

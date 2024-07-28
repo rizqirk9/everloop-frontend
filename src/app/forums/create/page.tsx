b05022077487eee31c5ad10aa0e0d5303bdf4cb3
Nashir Jamali <v-nashir.jamali@btpn.com>
2024-07-28 10:25:42 +0700

    fix error navigator

--- src/app/forums/create/page.tsx
+++ src/app/forums/create/page.tsx
@@ -1,13 +1,17 @@
 import { Container } from '@mui/material';
 import React from 'react';
 import Header from './_section/header';
-import Form from './_section/form'
+import Form from './_section/form';
 
 export default function CreateForum() {
-  return (
-    <Container>
-      <Header />
-      <Form />
-    </Container>
-  );
+  if (typeof navigator !== 'undefined') {
+    return (
+      <Container>
+        <Header />
+        <Form />
+      </Container>
+    );
+  } else {
+    return <></>;
+  }
 }

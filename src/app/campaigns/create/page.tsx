261980e4f4f890cdd57311d6e88343191bcd5d37
Nashir Jamali <v-nashir.jamali@btpn.com>
2024-07-28 10:45:47 +0700

    handle use client

--- src/app/campaigns/create/page.tsx
+++ src/app/campaigns/create/page.tsx
@@ -1,21 +1,17 @@
+'use client'
+
 import { Box, Container } from '@mui/material';
 import React from 'react';
 import Header from './_section/header';
 import Form from './_section/form';
 
 export default function Create() {
-  if (typeof navigator !== 'undefined') {
-    return (
-      <Container
-        sx={{
-          my: 10
-        }}
-      >
-        <Header />
-        <Form />
-      </Container>
-    );
-  } else {
-    return <></>;
-  }
+  <Container
+    sx={{
+      my: 10
+    }}
+  >
+    <Header />
+    <Form />
+  </Container>;
 }
